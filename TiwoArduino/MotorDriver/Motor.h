6dfd6a1543190e03c0deae20b8550fb121fa018b
Ivan <isvogor@foi.hr>
2015-04-25 11:36:23 +0200

    Changed numbers for AtMega

--- TiwoArduino/MotorDriver/Motor.h
+++ TiwoArduino/MotorDriver/Motor.h
@@ -12,15 +12,16 @@
 #include "AccelStepper.h"
 
 // pins
-static int const DIR_LEFT = 10;
-static int const PUL_LEFT = 11;
-static int const DIR_RIGHT = 13;
-static int const PUL_RIGHT = 12;
+static int const DIR_LEFT = 53;
+static int const PUL_LEFT = 51;
+
+static int const DIR_RIGHT = 47;
+static int const PUL_RIGHT = 49;
 
 // speeds
 static int const MAX_SPEED = 1700;//850; // max speed (maybe *2 because of halfstep)
 static int const DEF_ACCEL = 300; // default acceleration
-static int const DEF_SPEED = 600; // default speed
+static int const DEF_SPEED = 1200; // default speed
 
 class Motor {
 public:
